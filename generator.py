a04c97eb47c25f4215775f1b69a3de948b51b9eb
mits58 <ynakano927@gmail.com>
2020-03-14 15:50:41 +0900

    bug fixed

--- generator.py
+++ generator.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 from selfattention import SelfAttention
@@ -9,35 +10,40 @@ class Generator(nn.Module):
 
         self.layer1 = nn.Sequential(
             nn.utils.spectral_norm(nn.ConvTranspose2d(z_dim, image_size * 8,
-                                                      kernel_size=4),
+                                                      kernel_size=4)),
             nn.BatchNorm2d(image_size * 8),
-            nn.ReLU(inplace=True)))
+            nn.ReLU(inplace=True)
+        )
 
         self.layer2 = nn.Sequential(
             nn.utils.spectral_norm(nn.ConvTranspose2d(image_size * 8, image_size * 4,
-                                                      kernel_size=4, stride=2, padding=1),
+                                                      kernel_size=4, stride=2, padding=1)),
             nn.BatchNorm2d(image_size * 4),
-            nn.ReLU(inplace=True)))
+            nn.ReLU(inplace=True)
+        )
 
         self.layer3 = nn.Sequential(
             nn.utils.spectral_norm(nn.ConvTranspose2d(image_size * 4, image_size * 2,
-                                                      kernel_size=4, stride=2, padding=1),
+                                                      kernel_size=4, stride=2, padding=1)),
             nn.BatchNorm2d(image_size * 2),
-            nn.ReLU(inplace=True)))
+            nn.ReLU(inplace=True)
+        )
 
         self.selfattention1 = SelfAttention(input_dim=image_size * 2)
 
         self.layer4 = nn.Sequential(
             nn.utils.spectral_norm(nn.ConvTranspose2d(image_size * 2, image_size,
-                                                      kernel_size=4, stride=2, padding=1),
+                                                      kernel_size=4, stride=2, padding=1)),
             nn.BatchNorm2d(image_size),
-            nn.ReLU(inplace=True)))
+            nn.ReLU(inplace=True)
+        )
 
         self.selfattention2 = SelfAttention(input_dim=image_size)
 
         self.layer5 = nn.Sequential(
             nn.ConvTranspose2d(image_size, 1, kernel_size=4, stride=2, padding=1),
-            nn.Tanh())
+            nn.Tanh()
+        )
 
     def forward(self, z):
         out = self.layer1(z)
@@ -49,3 +55,18 @@ class Generator(nn.Module):
         out = self.layer5(out)
 
         return out, at_map1, at_map2
+
+
+if __name__ == '__main__':
+    # for debug
+    import matplotlib.pyplot as plt
+
+    G = Generator(z_dim=20, image_size=64)
+    input_rnd = torch.randn(1, 20)
+    input_rnd = input_rnd.view(1, 20, 1, 1)
+
+    output_image, _, _ = G(input_rnd)
+
+    img = output_image[0][0].detach().numpy()
+    plt.imshow(img, 'gray')
+    plt.show()
